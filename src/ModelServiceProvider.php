7433d7877f69af9c907c5c2d09283592dbf6b7c3
deepakdumraliya1998@gmail.com-at-608978162013 <deepakdumraliya1998@gmail.com>
2024-11-18 17:50:43 +0530

    Added Files

---
+++ src/ModelServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace IdQueue\IdQueuePackage;
+
+namespace IdQueue\IdQueuePackage\Commands;
+use IdQueue\IdQueuePackage\ModelLister;
+use Illuminate\Support\ServiceProvider;
+
+class ModelServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(ModelLister::class, function ($app) {
+            return new ModelLister;
+        });
+    }
+
+    public function boot()
+    {
+        // Load migrations from the package
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // Allow users to publish migrations to their project
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'id-queue-migrations');
+        if ($this->app->runningInConsole()) {
+
+            $this->commands([
+                ExportModelsCommand::class,
+            ]);
+        }
+    }
+}
